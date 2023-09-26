0181e4459ec401de464cbd83fc41209e420d13ce
Leo007ua <leo.oliinyk@ukr.net>
2023-09-27 00:06:30 +0300

    add controllers, midlewares

---
+++ controllers/contacts.js
@@ -0,0 +1,46 @@
+const contacts = require("../models/contacts");
+const { HttpError, CtrlWrapper } = require("../helpers");
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+const add = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+const deleteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  // res.status(204).send();
+  res.json({
+    message: "contact deleted",
+  });
+};
+const updateById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAll: CtrlWrapper(getAll),
+  getById: CtrlWrapper(getById),
+  add: CtrlWrapper(add),
+  deleteById: CtrlWrapper(deleteById),
+  updateById: CtrlWrapper(updateById),
+};
