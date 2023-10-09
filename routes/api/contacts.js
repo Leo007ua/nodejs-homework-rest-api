ffba73001d9d7466a9acfbcd3c4230cfc0444a6c
Leo007ua <leo.oliinyk@ukr.net>
2023-10-09 21:26:49 +0300

    add pagination

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,20 +4,32 @@ const ctrl = require("../../controllers/contacts");
 
 const schemas = require("../../schemas/contactsSchema");
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate} = require("../../middlewares");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.contactsSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.contactsSchema), ctrl.add);
 
-router.delete("/:contactId", isValidId, ctrl.deleteById);
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteById);
 
-router.put("/:contactId", isValidId, validateBody(schemas.contactsSchema), ctrl.updateById);
+router.put(
+  "/:contactId",
+  authenticate,
+  isValidId,
+  validateBody(schemas.contactsSchema),
+  ctrl.updateById
+);
 
-router.patch("/:contactId/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+router.patch(
+  "/:contactId/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
